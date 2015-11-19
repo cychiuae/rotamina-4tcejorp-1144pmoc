bd2c6f6e8ab5467b3929c5d3817af70eb887a7c6
YinYin Chiu <cychiuae@connect.ust.hk>
2015-11-19 18:23:16 +0800

    [Particle basic impl]

--- AniSkelF14/particle.cpp
+++ AniSkelF14/particle.cpp
@@ -1,1 +1,10 @@
  // SAMPLE_SOLUTION
+
+#include "particle.h"
+
+Particle::Particle() : m_fMass(0.0), m_position(Vec3f(0, 0, 0)), m_velocity(Vec3f(0, 0, 0)) {
+}
+
+Particle::Particle(float mass, const Vec3f &position, const Vec3f &velocity) : m_fMass(mass), m_position(position), m_velocity(velocity) {
+}
+
--- AniSkelF14/particle.h
+++ AniSkelF14/particle.h
@@ -1,1 +1,18 @@
  // SAMPLE_SOLUTION
+
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "vec.h"
+
+class Particle {
+public:
+	Particle();
+	Particle(float mass, const Vec3f &position, const Vec3f &velocity);
+private:
+	float m_fMass;
+	Vec3f m_position;
+	Vec3f m_velocity;
+};
+
+#endif
